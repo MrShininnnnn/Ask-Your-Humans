fbbdfded2da264ed2997bb63cf0240dede7d27b2
xingnanzhou <87948508+xingnanzhou@users.noreply.github.com>
2021-07-30 10:52:32 -0400

    Training IL with Generative Language (#8)
    * Split data to train and validation, use tensorboard to show curves
    
    * Log bleu score and token accuracy during training
    
    * Load args from yml file
    
    * Apply args_loader to train_instructions to allow running in different environment
    
    * Add a method to instructions generator to predict during eval
    
    * IL+Instructions generator model
    
    * restore instructions_discriminate_model
    
    * Train IL and Generative language together
    
    * Try teacher training, but predict instructions in test. Test loss is so high

--- apps/imitation_learning_with_generative_language_model.py
+++ apps/imitation_learning_with_generative_language_model.py
@@ -8,36 +8,38 @@ import torch.nn.functional as F
 
 
 class ImitationLearningWithGenerativeLanguageModel(nn.Module):
+  """ IL model but supports passing in hidden layer from LSTM. """
 
   def __init__(self,
-               instructions_generator_model,
                embedding_dim,
-               vocab,
+               # vocab,
                encoder_dim=128):
     super(ImitationLearningWithGenerativeLanguageModel, self).__init__()
 
     self.state_encoder = StateEncoder(embedding_dim, encoder_dim=encoder_dim)
-    self.instructions_generator = instructions_generator_model
-    self.vocab = vocab
+    # self.instructions_generator = instructions_generator_model
+    # self.vocab = vocab
 
     self.fc_state_encoder = nn.Linear(27 * 128, 48)
     self.fc_state_and_hidden = nn.Linear(48 + 32, 48)
     self.fc_final = nn.Linear(48, 9)
 
   def forward(self, grid_embedding, grid_onehot, inventory_embedding,
-              goal_embedding, instructions, instructions_lengths):
+              goal_embedding, lstm_hiddens=None):
     state = self.state_encoder(grid_embedding, grid_onehot, inventory_embedding,
                                goal_embedding)
-    self.instructions_generator.eval()
-    _, hiddens = self.instructions_generator.predict(grid_embedding,
-                                                     grid_onehot,
-                                                     inventory_embedding,
-                                                     goal_embedding, self.vocab)
+    # self.instructions_generator.eval()
+    # _, hiddens = self.instructions_generator.predict(grid_embedding,
+    #                                                  grid_onehot,
+    #                                                  inventory_embedding,
+    #                                                  goal_embedding, self.vocab)
 
     state = state.view(-1, state.size(1) * state.size(2))
     state = F.relu(self.fc_state_encoder(state))
 
-    state_and_hidden = torch.cat((state, hiddens), dim=1)
-    out = self.fc_state_and_hidden(state_and_hidden)
-    out = self.fc_final(out)
+    if lstm_hiddens is not None:
+      state = torch.cat((state, lstm_hiddens), dim=1)
+      state = self.fc_state_and_hidden(state)
+
+    out = self.fc_final(state)
     return out
