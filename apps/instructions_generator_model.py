fec2f8959783ddf01226624e643c09b070196419
Xingnan Zhou <superzxn0705@gmail.com>
2021-07-31 11:35:16 -0400

    Change always use teacher forcing to 0.5

--- apps/instructions_generator_model.py
+++ apps/instructions_generator_model.py
@@ -15,7 +15,7 @@ class InstructionsGeneratorModel(nn.Module):
 
   def __init__(self,
                device,
-               vocab_size,
+               vocab,
                embedding_dim,
                embed_weights,
                encoder_dim=128,
@@ -27,12 +27,13 @@ class InstructionsGeneratorModel(nn.Module):
     super(InstructionsGeneratorModel, self).__init__()
 
     self.device = device
+    self.vocab = vocab
 
     self.encoder = StateEncoder(embedding_dim, encoder_dim=encoder_dim)
     self.encoder_dim = encoder_dim
     self.decoder_dim = decoder_dim
     self.max_seq_length = max_seq_length
-    self.vocab_size = vocab_size
+    self.vocab_size = len(vocab)
     self.embedding_dim = embedding_dim
 
     self.attention = Attention(encoder_dim, decoder_dim, attention_dim)
@@ -40,18 +41,16 @@ class InstructionsGeneratorModel(nn.Module):
     self.fc_init_hidden = nn.Linear(encoder_dim, decoder_dim)
     self.fc_init_cell = nn.Linear(encoder_dim, decoder_dim)
 
-    # TODO(xingnanzhou): Change to embedding, need to train and save the model again
-    self.target_instructions_embedding = nn.Embedding(vocab_size, embedding_dim)
-    self.target_instructions_embedding.load_state_dict(
-        {'weight': embed_weights})
+    self.embedding = nn.Embedding(self.vocab_size, embedding_dim)
+    self.embedding.load_state_dict({'weight': embed_weights})
     if self.training:
-      self.target_instructions_embedding.weight.requires_grad = False
+      self.embedding.weight.requires_grad = False
 
     self.fc_beta = nn.Linear(decoder_dim, encoder_dim)
     self.sigmoid = nn.Sigmoid()
     self.decode_step = nn.LSTMCell(
         embedding_dim + encoder_dim, decoder_dim, bias=True)
-    self.fc = nn.Linear(decoder_dim, vocab_size)
+    self.fc = nn.Linear(decoder_dim, self.vocab_size)
     self.dropout = nn.Dropout(p=dropout)
 
   def init_hidden_state(self, encoder_out):
@@ -61,8 +60,14 @@ class InstructionsGeneratorModel(nn.Module):
     cell_state = self.fc_init_cell(mean_encoder_out)
     return hidden_state, cell_state
 
-  def forward(self, grid_embedding, grid_onehot, inventory_embedding,
-              goal_embedding, instructions, instructions_lengths):
+  def forward(self,
+              grid_embedding,
+              grid_onehot,
+              inventory_embedding,
+              goal_embedding,
+              instructions,
+              instructions_lengths,
+              use_teacher_forcing=False):
     encoder_out = self.encoder(grid_embedding, grid_onehot, inventory_embedding,
                                goal_embedding)
 
@@ -70,8 +75,6 @@ class InstructionsGeneratorModel(nn.Module):
 
     hidden_state, cell_state = self.init_hidden_state(
         encoder_out)  # (batch_size, max_caption_length, embed_dim)
-    target_instructions_embedding = self.target_instructions_embedding(
-        instructions)  # (batch_size, decoder_dim)
 
     # We won't decode at the <end> position, since we've finished generating as
     # soon as we generate <end>. So, decoding lengths are actual lengths - 1
@@ -85,26 +88,53 @@ class InstructionsGeneratorModel(nn.Module):
                          encoder_out.size(1)).to(self.device)
     hiddens = hidden_state.clone()
 
-    for t in range(max(decode_lengths)):
-      batch_size_t = sum([l > t for l in decode_lengths])
-      attention_weighted_encoding, alpha = self.attention(
-          encoder_out[:batch_size_t], hidden_state[:batch_size_t])
+    if use_teacher_forcing:
+      # target_instructions_embedding = self.embedding(
+      #     instructions)  # (batch_size, decoder_dim)
+      for t in range(max(decode_lengths)):
+        batch_size_t = sum([l > t for l in decode_lengths])
+        next_word = instructions[:batch_size_t, t]  # (64)
+        embedded = self.embedding(next_word)  # (64, 300)
+        # embedded = target_instructions_embedding[:batch_size_t, t, :]
 
-      gate = self.sigmoid(self.fc_beta(hidden_state[:batch_size_t]))
-      attention_weighted_encoding = gate * attention_weighted_encoding
+        attention_weighted_encoding, alpha = self.attention(
+            encoder_out[:batch_size_t], hidden_state[:batch_size_t])
 
-      decode_input = torch.cat([
-          target_instructions_embedding[:batch_size_t, t, :],
-          attention_weighted_encoding
-      ],
-                               dim=1)
-      hidden_state, cell_state = self.decode_step(
-          decode_input,
-          (hidden_state[:batch_size_t], cell_state[:batch_size_t]))
-      hiddens[:batch_size_t] = hidden_state.clone()
-      preds = self.fc(self.dropout(hidden_state))
-      predictions[:batch_size_t, t, :] = preds
-      alphas[:batch_size_t, t, :] = alpha
+        gate = self.sigmoid(self.fc_beta(hidden_state[:batch_size_t]))
+        attention_weighted_encoding = gate * attention_weighted_encoding
+
+        decode_input = torch.cat([embedded, attention_weighted_encoding], dim=1)
+        hidden_state, cell_state = self.decode_step(
+            decode_input,
+            (hidden_state[:batch_size_t], cell_state[:batch_size_t]))
+        hiddens[:batch_size_t] = hidden_state.clone()
+        preds = self.fc(self.dropout(hidden_state))
+        predictions[:batch_size_t, t, :] = preds
+        alphas[:batch_size_t, t, :] = alpha
+
+    else:
+      next_word = torch.LongTensor([[self.vocab.word2idx['<start>']]] *
+                                   batch_size).to(self.device)  # (64, 1)
+      for t in range(max(decode_lengths)):
+        batch_size_t = sum([l > t for l in decode_lengths])
+        next_word = next_word[:batch_size_t, 0]
+        embedded = self.embedding(next_word)  # (64, 1, 300)
+
+        attention_weighted_encoding, alpha = self.attention(
+            encoder_out[:batch_size_t], hidden_state[:batch_size_t])
+
+        gate = self.sigmoid(self.fc_beta(hidden_state[:batch_size_t]))
+        attention_weighted_encoding = gate * attention_weighted_encoding
+
+        decode_input = torch.cat([embedded, attention_weighted_encoding], dim=1)
+        hidden_state, cell_state = self.decode_step(
+            decode_input,
+            (hidden_state[:batch_size_t], cell_state[:batch_size_t]))
+        hiddens[:batch_size_t] = hidden_state.clone()
+        preds = self.fc(self.dropout(hidden_state))
+        predictions[:batch_size_t, t, :] = preds
+        alphas[:batch_size_t, t, :] = alpha
+        _, next_word = preds.topk(1)
 
     return predictions, decode_lengths, alphas, hiddens
 
@@ -125,7 +155,7 @@ class InstructionsGeneratorModel(nn.Module):
     hiddens = hidden_state.clone()
 
     while True:
-      embeddings = self.target_instructions_embedding(k_prev_words).squeeze(1)
+      embeddings = self.embedding(k_prev_words).squeeze(1)
       attention_weighted_encoding, _ = self.attention(encoder_out, hidden_state)
       gate = self.sigmoid(self.fc_beta(hidden_state))
       attention_weighted_encoding = gate * attention_weighted_encoding
